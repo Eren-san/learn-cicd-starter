4a9031a68ac4d5083273e4305274d6caf97b50aa
eren <eren.kilic_25@hotmail.com>
2025-12-11 22:54:51 +0300

    fix sex issue

--- json.go
+++ json.go
@@ -30,5 +30,9 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 		return
 	}
 	w.WriteHeader(code)
-	w.Write(dat)
+
+	// G104 Düzeltmesi: w.Write() hatası kontrol ediliyor
+	if _, err := w.Write(dat); err != nil {
+		log.Printf("%v", err)
+	}
 }
